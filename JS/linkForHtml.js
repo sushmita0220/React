cadf73b714ecd2309ec1618ba5e95071207ca7a4
Sushmita V <sushmitaa.venkataraman@gmail.com>
2023-08-03 16:31:45 +0530

    created

--- JS/linkForHtml.js
+++ JS/linkForHtml.js
@@ -1,1 +1,15 @@
-document.getElementById("hello");
+alert("Dont open this page")
+window.print("")
+
+function myFunfor1st(){
+    document.getElementById("hello").innerHTML = "I am in first year"
+}
+function myFunfor2nd(){
+    document.getElementById("hello").innerHTML = "I am in second year"
+}
+function myFunfor3rd(){
+    document.getElementById("hello").innerHTML = "I am in third year"
+}
+function myFunforfinal(){
+    document.getElementById("hello").innerHTML = "I am in final year"
+}

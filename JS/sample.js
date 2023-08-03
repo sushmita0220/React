fc414f6911e3a5ef37e9f4fb9654ba0aa16dec2b
Sushmita V <sushmitaa.venkataraman@gmail.com>
2023-08-03 14:26:53 +0530

    updated

--- JS/sample.js
+++ JS/sample.js
@@ -39,7 +39,40 @@ while(i <=a){
     console.log(i)
     i++
 }
-array = [10,20]
-array.forEach(element => {
-    console.log(element)
-});
+//array = [10,20]
+//array.forEach(element => {
+  //  console.log(element)
+//});
+const array = [10,20,"KSP",23.08,Date()]
+for (var ele in array){
+    console.log(array[ele])
+}
+
+
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+console.log("After popping:")
+
+if(array.length>0){
+    console.log("elements are present")
+    for(var ele of array){
+        console.log(ele)
+    }
+}
+else{
+    console.log("elements are not present")
+}
+array.push(1)
+if(array.length>0){
+    console.log("elements are present")
+    for(var ele of array){
+        console.log(ele)
+    }
+}
+else{
+    console.log("elements are not present")
+}

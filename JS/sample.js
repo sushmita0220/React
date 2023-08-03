d0f85f57bf30efda303b1bf3a47995e860bd9808
Sushmita V <sushmitaa.venkataraman@gmail.com>
2023-08-03 15:09:54 +0530

    updated

--- JS/sample.js
+++ JS/sample.js
@@ -76,3 +76,44 @@ if(array.length>0){
 else{
     console.log("elements are not present")
 }
+//OBJECT
+a = {
+    car: "Audi",
+    model: "A8",
+    airbags:8,
+    wheels:5,
+    color:"blue"
+}
+console.log(a['color'])
+console.log(a.airbags)
+var some = 'model'
+console.log(a[some])
+console.log(a)
+a.car = "BMW"
+console.log(a)
+a.sunroof = "Yes"
+console.log(a)
+
+//NESTED OBJS
+console.log(a['color'])
+console.log(a.airbags)
+var some = 'model'
+console.log(a[some])
+console.log(a)
+a.car = "BMW"
+console.log(a)
+a.sunroof = {isPresent:"YES",Shape:"Rectangle"}
+console.log(a)
+console.log(a.sunroof.isPresent)
+for(var ele in a){
+    console.log(ele+ ":"+ a[ele])
+    if(typeof a[ele]=="object")
+    {
+        for(var val in a[ele]){
+            console.log(val +":"+a[ele][val])
+        }
+    }
+}
+
+
+

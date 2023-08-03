9a6b1de544ffeb1c1992421a9fc7a561c8252efe
Sushmita V <sushmitaa.venkataraman@gmail.com>
2023-08-03 11:32:51 +0530

    updated

--- JS/sample.js
+++ JS/sample.js
@@ -1,16 +1,16 @@
-//var a =120;
-//console.log("This is value declared in var type before altering:"+""+a);  
-//{
-  //  var a =7
-    //console.log("This is value declared in var type after altering:"+""+a);  
-//}
+var a =120;
+console.log("This is value declared in var type before altering:"+""+a);  
+{
+    var a =7
+    console.log("This is value declared in var type after altering:"+""+a);  
+}
 
-//let a= 120;
-//console.log("This is value declared in var type before altering:"+""+a);  
-//{
-  //  let a =7
-    //console.log("This is value declared in var type after altering:"+""+a);  
-//}
-//console.log("This is value declared in var type after block altering:"+""+a);  
+let a= 120;
+console.log("This is value declared in var type before altering:"+""+a);  
+{
+    let a =7
+    console.log("This is value declared in var type after altering:"+""+a);  
+}
+console.log("This is value declared in var type after block altering:"+""+a);  
 const c =20;
 console.log("This is value declared in const type:"+""+c); 
